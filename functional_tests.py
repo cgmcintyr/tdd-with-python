77ab665b0897d2b8683e27b2326ff6e5a219afdb
Christopher McIntyre <me@cgmcintyre.com>
2017-06-21 14:32:39 +0100

    4 FT checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 def get_firefox_driver():
@@ -13,7 +14,7 @@ def get_firefox_driver():
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = get_firefox_driver()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(1)
 
     def tearDown(self):
         self.browser.quit()
@@ -25,16 +26,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # She is invited to enter her to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholer'),
+                'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the pages lists
         # "1: Buy peacock feathers as an itme in a to-do list"
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
